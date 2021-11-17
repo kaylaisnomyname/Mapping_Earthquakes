dacf0553fa9b6096353f85e81a639d099cb8a380
tdcqzyzs d <loginplz@LOGINtekiMacBook-Pro.local>
2021-11-17 18:57:16 -0500

    adding Mapping GeoJSON Points

---
+++ Mapping_GeoJSON_Points/logic.js
@@ -0,0 +1,72 @@
+  /* Mapping Lines */
+
+// Add console.log to check to see if our code is working.
+console.log("Mapping_GeoJSON_Points is working");
+
+// Create the map object with a center among the airports
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection",
+"features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+    "geometry":{
+      "type":"Point",
+      "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// note: GeoJSON coordinates [lng, lat]
+
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// //grabbing GeoJSON data and add a marker on [lat,lng], using pointToLayer method
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng).bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" +feature.properties.city + ", " + feature.properties.country+"</h3>");
+//   }
+// }).addTo(map);
+
+
+// // map tileLayer
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+
+// add a popup marker using onEachFeature funtion
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2> Airport code: " + feature.properties.faa + "</h2> <hr> <h3> Airport name: " +feature.properties.name + "</h3>");
+  }
+}).addTo(map);
+// map tileLayer
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
